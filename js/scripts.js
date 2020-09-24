3789f00e63e4fa4deffdfd8a24fe0b14f2df2f94
Hyun Young Lee <60396795+hylee-vx@users.noreply.github.com>
2020-09-24 12:27:28 +0100

    Created var pokemonList
    - created array of objects

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+let pokemonList = [
+  {name: 'Bulbasaur', height: 0.7, weight: 6.9, types:['grass', 'poison']},
+  {name: 'Weedle', height: 0.3, weight: 3.2, types:['bug', 'poison']},
+  {name: 'Pidgey', height: 0.3, weight: 1.8, types: ['flying', 'normal']},
+  {name: 'Fearow', height: 1.2, weight: 38, types: ['flying', 'normal']},
+  {name: 'Nidoqueen', height: 1.3, weight: 60, types: ['ground', 'poison']}
+];
