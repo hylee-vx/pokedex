edf94e91847f1ce86573a7f0f8edc08a81d37643
Hyun Young Lee <60396795+hylee-vx@users.noreply.github.com>
2020-09-28 00:29:15 +0100

    Added for loop to list pokemon name and height
    - included if-else statement to highlight the tallest pokemon
    - added id="big-height" to string highlighting the tallest pokemon for css styling

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,18 @@ let pokemonList = [
   {name: 'Fearow', height: 1.2, weight: 38, types: ['flying', 'normal']},
   {name: 'Nidoqueen', height: 1.3, weight: 60, types: ['ground', 'poison']}
 ];
+
+//Shows each pokemon's name and height
+for (let i = 0; i < pokemonList.length; i++) {
+  let pokemonHeight = '<h2>' + pokemonList[i].name + '</h2>' +
+  '<p>' + 'Height: ' + pokemonList[i].height + 'm' + '</p>';
+  //Highlights tallest pokemon
+  if (pokemonList[i].height < 2) {
+    document.write(pokemonHeight)
+  } else if (pokemonList[i].height >= 2) {
+    document.write(
+      pokemonHeight +
+      '<span id="big-height">' + 'Wow, that\'s big!' + '</span>'
+    )
+  }
+}
