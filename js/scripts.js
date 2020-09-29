59e9fbb157b85b123b25119408f54fd14029619f
Hyun Young Lee <60396795+hylee-vx@users.noreply.github.com>
2020-09-29 12:38:33 +0100

    Deleted for loop with if-else statement

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,6 @@ let pokemonList = [
   {name: 'Nidoqueen', height: 1.3, weight: 60, types: ['ground', 'poison']}
 ];
 
-//Shows each pokemon's name and height
-for (let i = 0; i < pokemonList.length; i++) {
-  let pokemonHeight = '<h2>' + pokemonList[i].name + '</h2>' +
-  '<p>' + 'Height: ' + pokemonList[i].height + 'm' + '</p>';
-  //Highlights tallest pokemon
-  if (pokemonList[i].height < 2) {
     document.write(pokemonHeight)
-  } else if (pokemonList[i].height >= 2) {
-    document.write(
-      pokemonHeight +
-      '<span id="big-height">' + 'Wow, that\'s big!' + '</span>'
-    )
   }
 }
