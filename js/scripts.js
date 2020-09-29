f0d39c0886ba67c0eb70452361ccd03b38b75c84
Hyun Young Lee <60396795+hylee-vx@users.noreply.github.com>
2020-09-29 13:45:07 +0100

    Added var pokemonList to IIFE held in var pokemonRepository

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,13 @@
-let pokemonList = [
-  {name: 'Bulbasaur', height: 0.7, weight: 6.9, types:['grass', 'poison']},
-  {name: 'Weedle', height: 0.3, weight: 3.2, types:['bug', 'poison']},
-  {name: 'Onix', height: 8.8, weight: 210, types: ['rock', 'ground']},
-  {name: 'Pidgey', height: 0.3, weight: 1.8, types: ['flying', 'normal']},
-  {name: 'Fearow', height: 1.2, weight: 38, types: ['flying', 'normal']},
-  {name: 'Nidoqueen', height: 1.3, weight: 60, types: ['ground', 'poison']}
-];
+let pokemonRepository = (function() {
+  let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, weight: 6.9, types:['grass', 'poison']},
+    {name: 'Weedle', height: 0.3, weight: 3.2, types:['bug', 'poison']},
+    {name: 'Onix', height: 8.8, weight: 210, types: ['rock', 'ground']},
+    {name: 'Pidgey', height: 0.3, weight: 1.8, types: ['flying', 'normal']},
+    {name: 'Fearow', height: 1.2, weight: 38, types: ['flying', 'normal']},
+    {name: 'Nidoqueen', height: 1.3, weight: 60, types: ['ground', 'poison']}
+  ];
+})();
 
 function getPokemonHeight(character) {
   //Shows each pokemon's height
