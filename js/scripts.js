cdb96d929d0d19dbedc02a0f664b8113617f71a6
Hyun Young Lee <60396795+hylee-vx@users.noreply.github.com>
2020-10-06 12:13:30 +0100

    Replaced var repository with empty array
    - data to come from API

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,5 @@
 let pokemonRepository = (function() {
-  let repository = [
-    {name: 'Bulbasaur', height: 0.7, weight: 6.9, types:['grass', 'poison']},
-    {name: 'Weedle', height: 0.3, weight: 3.2, types:['bug', 'poison']},
-    {name: 'Onix', height: 8.8, weight: 210, types: ['rock', 'ground']},
-    {name: 'Pidgey', height: 0.3, weight: 1.8, types: ['flying', 'normal']},
-    {name: 'Fearow', height: 1.2, weight: 38, types: ['flying', 'normal']},
-    {name: 'Nidoqueen', height: 1.3, weight: 60, types: ['ground', 'poison']}
-  ];
+  let repository = [];
 
   function add(pokemon) {
     if
