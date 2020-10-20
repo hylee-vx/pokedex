2b4fba101841a688eb3eef7b8af37efbb2023829
Hyun Young Lee <60396795+hylee-vx@users.noreply.github.com>
2020-10-21 00:05:27 +0100

    Updated var pokemonList and pokemonListItem with jQuery and const
    - also updated class names to reflect Bootstrap classes

--- js/scripts.js
+++ js/scripts.js
@@ -17,14 +17,15 @@ const pokemonRepository = (function() {
   //Creates list of pokemon from API loaded via showDetails() to be displayed inside buttons
   //Displays details in modal with click event
   function addListItem(pokemon) {
-    let pokemonList = document.querySelector('.pokemon-list');
-    let pokemonListItem = document.createElement('li');
     let button = document.createElement('button');
     button.innerText = pokemon.name;
     button.classList.add('button-standard');
     pokemonListItem.appendChild(button);
     pokemonList.appendChild(pokemonListItem);
     button.addEventListener('click', function() {
+    const pokemonList = $('.list-group');
+    const pokemonListItem = $('<li></li>');
+    pokemonListItem.addClass('group-list-item');
       showDetails(pokemon);
       });
     }
